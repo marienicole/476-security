79e43e29c4357ed5322e977a9ddebde2dfcf4728
wizmariefa <mariecolee1@gmail.com>
2019-03-05 20:45:06 -0700

    actual necessary additions

--- assn2/morin.py
+++ assn2/morin.py
@@ -1,5 +1,10 @@
-import base64
-from pprint import pprint
+'''
+Marie Morin
+Security Assignment #2
+To use: run program in the same directory as the memorydump.dmp file.
+
+This is written in Python (rather than Java), as per approval of Dr. Binhai Zhu.
+'''
 with open("memorydump.dmp", "rb") as f:
     dump = f.read()
     dump = "".join(chr(x) for x in bytearray(dump))
@@ -12,16 +17,25 @@ for entry in dump:
         current = entry.split("^") # split on field separators
         if len(current[0]) > 19 or len(current[1]) > 26:
             continue
-        card_no = current[0]
+        card_no_temp = current[0]
+        card_no = ""
+        for i in range(len(card_no_temp)):
+            if i % 4 == 0:
+                card_no += ' '
+            card_no += card_no_temp[i]
+
         name = current[1].replace("/", ", ")
         year = current[2][:2]
         month = current[2][2:4]
         cvv = current[2] [4:7]
-        cc_info = "\tCardholder's Name: %s\n\tCard Number: %s\n\tExpiration Date: %s/%s\n\tCVV Number: %s" % (
+        cc_info = "\tCardholder's Name: %s\n\tCard Number: %s\n\tExpiration Date: %s/20%s\n\tCVV Number: %s" % (
             name, card_no, month, year, cvv)
         cards.append(cc_info)
 i = 1
-for card in cards:
-    print("<Information of record #%i>" % i)
-    print(card)
-    i += 1
+with open("morin.output", "w+") as f:
+    f.write("There are %s track records in the memory data.\n" % len(cards))
+    for card in cards:
+        f.write("< Information of record #%i >\n" % i)
+        f.write(card)
+        f.write("\n")
+        i += 1
---
+++ assn3/morin.py
@@ -0,0 +1,48 @@
+import hashlib, linecache
+from datetime import datetime
+from multiprocessing import Process
+# title = "  ██▓███   █     █░    ▄████▄   ██▀███   ▄▄▄       ▄████▄   ██ ▄█▀▓█████  ██▀███\n"
+# title += "▓██░  ██▒▓█░ █ ░█░   ▒██▀ ▀█  ▓██ ▒ ██▒▒████▄    ▒██▀ ▀█   ██▄█▒ ▓█   ▀ ▓██ ▒ ██▒\n"
+# title += "▓██░ ██▓▒▒█░ █ ░█    ▒▓█    ▄ ▓██ ░▄█ ▒▒██  ▀█▄  ▒▓█    ▄ ▓███▄░ ▒███   ▓██ ░▄█ ▒\n"
+# title += "▒██▄█▓▒ ▒░█░ █ ░█    ▒▓▓▄ ▄██▒▒██▀▀█▄  ░██▄▄▄▄██ ▒▓▓▄ ▄██▒▓██ █▄ ▒▓█  ▄ ▒██▀▀█▄  \n"
+# title += "▒██▒ ░  ░░░██▒██▓    ▒ ▓███▀ ░░██▓ ▒██▒ ▓█   ▓██▒▒ ▓███▀ ░▒██▒ █▄░▒████▒░██▓ ▒██▒\n"
+# title += "▒▓▒░ ░  ░░ ▓░▒ ▒     ░ ░▒ ▒  ░░ ▒▓ ░▒▓░ ▒▒   ▓▒█░░ ░▒ ▒  ░▒ ▒▒ ▓▒░░ ▒░ ░░ ▒▓ ░▒▓░\n"
+# title += "░▒ ░       ▒ ░ ░       ░  ▒     ░▒ ░ ▒░  ▒   ▒▒ ░  ░  ▒   ░ ░▒ ▒░ ░ ░  ░  ░▒ ░ ▒░\n"
+# title += "░░         ░   ░     ░          ░░   ░   ░   ▒   ░        ░ ░░ ░    ░     ░░   ░ "
+# print(title)                                                       
+def run_tests(filename, hashes):
+    print("[*] Parsing passwords ... ")
+    i = 1
+    while linecache.getline(filename, i) != '':
+        pw = linecache.getline(filename, i).rstrip("\n")
+        print(pw)
+        if len(hashes) == 0:
+            print("[*] No hashes left in hash list! Exiting ...")
+            break
+        
+        starttime = datetime.now()
+        current_pass = pw.rstrip("\n")
+        pass_hash = hashlib.md5(current_pass.encode()).hexdigest()
+        if pass_hash in hashes:
+            diff = ((datetime.now()-starttime).microseconds)/1000
+            string = "The password for hash value %s is %s. " % (pass_hash, current_pass)
+            string += "It takes the program %s sec to recover this password." % (diff)
+            hashes.remove(pass_hash)
+            print(string)
+        linecache.clearcache()
+        i += 1
+
+if __name__ == '__main__':
+    start_time = datetime.now()
+    hashes = ["6f047ccaa1ed3e8e05cde1c7ebc7d958",
+            "275a5602cd91a468a0e10c226a03a39c",
+            "b4ba93170358df216e8648734ac2d539",
+            "dc1c6ca00763a1821c5af993e0b6f60a",
+            "8cd9f1b962128bd3d3ede2f5f101f4fc",
+            "554532464e066aba23aee72b95f18ba2"]
+    print("[*] Hashes loaded ...")
+
+    filename = 'crackstation-human-only.txt'
+    run_tests(filename, hashes)
+    total_time = datetime.now() - start_time
+    print("Total time running: %s" % total_time)
